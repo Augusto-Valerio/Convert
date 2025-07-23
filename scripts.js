e33c30ed9b2c99770363d3dba5ac178d7eedb2cd
Augusto Valerio <augusto.valerio06@gmail.com>
2025-07-22 22:00:58 -0300

    adding variable to quote

--- scripts.js
+++ scripts.js
@@ -1,3 +1,8 @@
+// Currency quote of the day
+const USD = 5.57
+const EUR = 6.54
+const GBP = 7.53
+
 const amount = document.getElementById("amount");
 
 // Manipulating the input amount to receive only numbers.
