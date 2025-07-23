329cd55fd85dcc54d52119662cb7a60486a87602
Augusto Valerio <augusto.valerio06@gmail.com>
2025-07-22 22:10:28 -0300

    getting the selected currency

--- scripts.js
+++ scripts.js
@@ -1,9 +1,11 @@
 // Currency quote of the day
-const USD = 5.57
-const EUR = 6.54
-const GBP = 7.53
+const USD = 5.57;
+const EUR = 6.54;
+const GBP = 7.53;
 
+const form = document.querySelector("form");
 const amount = document.getElementById("amount");
+const currency = document.getElementById("currency");
 
 // Manipulating the input amount to receive only numbers.
 amount.addEventListener("input", () => {
@@ -11,3 +13,9 @@ amount.addEventListener("input", () => {
   amount.value = amount.value.replace(hasCharactersRegex, "");
 });
 
+// Capturing the form's submit event.
+form.onsubmit = (event) => {
+  event.preventDefault();
+
+    console.log(currency.value)
+};
